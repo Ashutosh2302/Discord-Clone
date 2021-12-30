5ff6009b2ea31f7e8178435796ec4a2eaa0b29ed
Ashutosh <ashutoshb2000@gmail.com>
2021-12-30 18:38:04 +0530

    user registration

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q 
 from .models import *
 from .forms import RoomForm
@@ -17,7 +18,8 @@ from .forms import RoomForm
 # ]
 
 def login_page(request):
-    context = {}
+    page = 'login'
+    context = {'page': page}
 
     if request.user.is_authenticated:
         return redirect('home')
@@ -44,6 +46,21 @@ def login_page(request):
 def logout_user(request):
     logout(request)
     return redirect('home')
+
+def register_user(request):
+    form = UserCreationForm()
+    context = {'form': form}
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') else '' 
