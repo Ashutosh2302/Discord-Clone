b9c6bed064f750cc2eb2520be524262c14350e8b
Ashutosh <ashutoshb2000@gmail.com>
2022-01-05 02:05:19 +0530

    user profilr

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('profile/<str:pk>/', views.user_profile, name='profile'),
+
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create_room/', views.create_room, name='create-room'),
