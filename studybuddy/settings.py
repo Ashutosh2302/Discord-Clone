ebf76e095e0cc276c3a968d0b578922500cb4ce4
Ashutosh <ashutoshb2000@gmail.com>
2022-01-11 20:23:27 +0530

    heroku bug fix

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-@pqav=kw+#bajyu9e+lp6&u@ctf6o9%ci1cdkv2bv9z_g1n%dg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
