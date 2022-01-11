a975411b4f3808064ce4b200f96e8325893ae9e0
Ashutosh <ashutoshb2000@gmail.com>
2022-01-11 20:42:43 +0530

    heroku bug fix

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'studybuddy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('NAME'),
-        'HOST':  env('HOST'),
+        'NAME': 'd7i2dhd17t6aqb',
+        'HOST':  'ec2-34-230-198-12.compute-1.amazonaws.com',
         'PORT': 5432,
-        'USER':  env('USER'),
-        'PASSWORD': env('PASSWORD'),
+        'USER': 'sxbakfaipbrmzk',
+        'PASSWORD': '47d393179ce3f093c4d0c3eb6d5a5b7355b4fdae00dd3ccc2c9f6f39f24eac7c',
     }
 }
 
